3858baf7eccdcf34187f1624b1b5ddd29904b919
Prashant Suthar <prashant.suthar@zealousys.com>
2025-01-27 15:31:32 +0530

    update

--- setupNewProject.sh
+++ setupNewProject.sh
@@ -7,15 +7,21 @@ npm i && \
 
 # Update dependencies
 npm update && \
+
 # install pm2
 npm i pm2 && \
+
 # Install logrotate
 npm run install-logrotate && \
+
 # Format code
 npm run format
+
 # remove pm2-logrotate
 pm2 delete pm2-logrotate
+
 # start project
 # npm run start
+
 # # Remove the script file itself
 # rm -- "$0"
